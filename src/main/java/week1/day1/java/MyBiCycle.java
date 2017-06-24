91950b8cf8382ad86341ca9f70ad8fb15adf806c
Gopinath Jayakumar <gopinath@testleaf.com>
2017-06-24 20:15:46 +0530

    Week1Day1

---
+++ src/main/java/week1/day1/java/MyBiCycle.java
@@ -0,0 +1,15 @@
+package week1.day1.java;
+
+public class MyBiCycle {
+
+	public static void main(String[] args) {
+
+		Bicycle myCycle =  new Bicycle();
+		/*	myCycle.applyBrake();*/
+		
+		myCycle.isPuncture(true);
+		System.out.println(myCycle.wheels);
+
+	}
+
+}
